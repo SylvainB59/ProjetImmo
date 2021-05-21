4cba2c0f6fcfd5cf3d8e15c7d6eb2501cdd75a36
Syl <s.baillot59@gmail.com>
2021-05-21 15:10:23 +0200

    petit fix verif

--- classes/controller/inscription.php
+++ classes/controller/inscription.php
@@ -15,18 +15,17 @@ ViewTemplate::menu();
 			ViewTemplate::alert('Mail existe déjà.', 'warning', '#', 'Retour');
 		}else{
 			$donnees = [$_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['tel'], $_POST['pass']];
-	        $types = "nom","prenom","email","tel","pass"];
+	        $types = ["nom","prenom","email","tel","pass"];
 
 	        $data = Utils::valider($donnees, $types);
 	        if($data){
 				$token = base_convert(hash('sha256', time() . mt_rand()), 16, 36);
-				ModelUser::addUser($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['pass'], $_POST['tel'], $token);
+				// ModelUser::addUser($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['pass'], $_POST['tel'], $token);
 				ViewTemplate::alert('Inscription enregistrée, veuillez suivre le lien pour la valider.', 'success', 'confirmationMail.php?mail='.$_POST['mail'].'&token='.$token, 'Continuer');
 	        }
 		}
-	}else{
-		ViewUser::addUser();
 	}
+	ViewUser::addUser();
 	?>
 </body>
 <?php 
